860438db66e91f7797a04ef9b5eac365b120ae1a
Nemerd <xtremestripedtiger@gmail.com>
2022-07-04 19:30:27 -0300

    Areglados los estilos de los Items

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,11 +4,11 @@ import Item from "./Item";
 function ItemList({item}) {
 
     return (
-        <>
+        <div style={{display: "flex", flexFlow: "row", flexWrap: "wrap"}}>
         {item.map( x => <Item id={x.id} title={x.title}
             price={x.price} pictureUrl={x.pictureUrl} />
             )}
-        </>
+        </div>
     )
 }
 
